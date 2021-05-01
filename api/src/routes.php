7e6d6b1bf3026e9f7fa00eee0c6ef6cc9055ad57
Daniel Tiringer <tiringerdaniel@gmail.com>
2021-05-01 07:53:17 +0200

    Implement convert route, make template

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Magnet\MagnetConvert;
 use App\Magnet\MagnetIndex;
 
 $app->get('/', MagnetIndex::class)->setName('index');
+$app->post('/', MagnetConvert::class)->setName('convert');
