5fc9c138c52c827ba3be50e756a263b78ee67ded
Daniel Tiringer <tiringerdaniel@gmail.com>
2021-05-01 06:04:47 +0200

    Add twig templating, render simple view

--- api/public/index.php
+++ api/public/index.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -12,9 +14,15 @@ $app = AppFactory::create();
 
 $app->addErrorMiddleware(true, true, false);
 
+$twig = Twig::create(
+    __DIR__ . '/../templates/',
+    ['cache' => false]
+);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'main.twig');
 });
 
 $app->run();
