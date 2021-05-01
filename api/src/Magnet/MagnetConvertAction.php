178e1128a794244f9a6debe0ad2091fa89e11add
Daniel Tiringer <tiringerdaniel@gmail.com>
2021-05-01 19:30:50 +0200

    Add form validation

--- api/src/Magnet/MagnetConvertAction.php
+++ api/src/Magnet/MagnetConvertAction.php
@@ -7,19 +7,42 @@ use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface;
+use Respect\Validation\Exceptions\ValidatorException;
+use Respect\Validation\Validator;
 use Slim\Views\Twig;
 
 class MagnetConvertAction
 {
     public function __invoke(Request $request, Response $response, $args)
     {
+        $view = Twig::fromRequest($request);
+
         $data = $request->getParsedBody();
+
+        $validator = new Validator();
+        $validator->addRule(
+            Validator::key(
+                'magnet',
+                Validator::startsWith('magnet:?xt=urn:btih:')->setTemplate('The magnet\'s format is incorrect')
+            )->setTemplate('The magnet link is required')
+        );
+
+        try {
+            $validator->assert($data);
+        } catch (ValidatorException $e) {
+            return $view->render(
+                $response,
+                'magnet/index.html.twig',
+                [
+                    'errors' => $e->getMessages(),
+                ]
+            );
+        }
 
         $httpClient = new Client();
         $apiClient = new WebmasterApiClient($httpClient);
         $apiResponse = $apiClient->fetchTorrentLink($data['magnet']);
 
-        $view = Twig::fromRequest($request);
 
         if ($apiResponse['code'] === 1) {
             return $view->render(
